6bbc40c84a1cd89db4b15f1212d407dea2808fb2
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-09-17 18:58:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
-# Your code here
+# Meal choice for a wedding
+
+def meal_choice (veg1, veg2, protein)
+  puts "What a nutritional meal !!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice ("Fish", "Brocolli", "Asparagus")
+meal_choice (protein = "Meat", "Cauliflower", "Spinach")
